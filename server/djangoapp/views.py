f72a13ab817f7ef39284581fda0fe0e100b1fa24
Tory DeMaio <torydemaio@gmail.com>
2023-09-29 00:48:39 -0400

    added views and urls for signup, registration, login/logout. Also added form for login and edited templates to show approiate nav for authenticated users

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -36,14 +36,46 @@ def contact(request):
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
 # ...
+def login_request(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/index.html', context)
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("djangoapp:index")
+        else:
+            return redirect("djangoapp:index") 
 
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
 # ...
-
+def logout_request(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/index.html', context)
+    if request.method == "POST":
+        logout(request)
+        return redirect("djangoapp:index")
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
 # ...
+def registration_request(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/registration.html', context)
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+
+        user = User.objects.create_user(username=username, password=password, first_name=first_name, last_name=last_name)
+        user.save()
+        return redirect("djangoapp:index")
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
